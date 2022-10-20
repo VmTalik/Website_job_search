9f98a7da7a56aed89510d36558d5db99742a14fa
VmTalik <vmarlyan@mail.ru>
2022-10-20 23:13:15 +0300

    Add models

--- job_search/main/models.py
+++ job_search/main/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=30, db_index=True, verbose_name='Название города', unique=True)
+    slug = models.CharField(max_length=30, blank=True, unique=True)
+
+    class Meta:
+        verbose_name = 'город'
+        verbose_name_plural = 'Города'
+
+    def __str__(self):
+        return self.name
+
+
+class ProgrammingLanguage(models.Model):
+    name = models.CharField(max_length=30, db_index=True, verbose_name='Язык программирования', unique=True)
+    slug = models.CharField(max_length=30, blank=True, unique=True)
+
+    class Meta:
+        verbose_name = 'язык программирования'
+        verbose_name_plural = 'Языки программирования'
+
+    def __str__(self):
+        return self.name
