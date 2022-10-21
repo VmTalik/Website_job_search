cb53a84f75d0de47dc55099f79b59dbe7262a768
VmTalik <vmarlyan@mail.ru>
2022-10-21 15:24:18 +0300

    Register vacancy model

--- job_search/main/admin.py
+++ job_search/main/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import City, ProgrammingLanguage
+from .models import City, ProgrammingLanguage, Vacancy
+from django.contrib.admin.models import LogEntry
 
-admin.site.register([City, ProgrammingLanguage])
+admin.site.register([City, ProgrammingLanguage, Vacancy])
+
+#Удаление последних действий в админ панеле
+#LogEntry.objects.all().delete()
