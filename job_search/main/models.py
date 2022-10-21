d21410974ff28a90a8975275a835b6193de361ae
VmTalik <vmarlyan@mail.ru>
2022-10-21 15:16:19 +0300

    Add vacancy model

--- job_search/main/models.py
+++ job_search/main/models.py
@@ -34,3 +34,20 @@ class ProgrammingLanguage(models.Model):
         if not self.slug:
             self.slug = translation_slag(str(self.name))
         super().save(*args, **kwargs)
+
+
+class Vacancy(models.Model):
+    city = models.ForeignKey('City', on_delete=models.CASCADE, verbose_name='Город')
+    language = models.ForeignKey('ProgrammingLanguage', on_delete=models.CASCADE, verbose_name='Язык программирования')
+    url = models.URLField(unique=True)
+    title = models.CharField(max_length=200, db_index=True, verbose_name='Заголовок вакансии')
+    company = models.CharField(max_length=200, verbose_name='Компания')
+    description = models.TextField('Описание вакансии')
+    vacancy_date = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'вакансия'
+        verbose_name_plural = 'вакансии'
+
+    def __str__(self):
+        return self.title
