8eb45b808452c44f524eea105d1c877d29461a9a
VmTalik <vmarlyan@mail.ru>
2022-10-22 15:44:38 +0300

    Add a controller function for the main page

--- job_search/main/views.py
+++ job_search/main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Vacancy
 
-# Create your views here.
+
+def vacancies_view(request):
+    qs = Vacancy.objects.all()
+    context = {'object_list': qs}
+    return render(request, 'main/general.html', context)
