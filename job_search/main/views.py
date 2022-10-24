4b3fef888d83fc9e1d25ffe2694c39130a3c0d1e
VmTalik <vmarlyan@mail.ru>
2022-10-24 22:25:21 +0300

    Add vacancy filtering and search form view

--- job_search/main/views.py
+++ job_search/main/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
+
+from .forms import VacancyFindForm
 from .models import Vacancy
 
 
 def vacancies_view(request):
-    qs = Vacancy.objects.all()
-    context = {'object_list': qs}
+    city = request.GET.get('city')
+    language = request.GET.get('language')
+    qs = []
+    if city or language:
+        d_choice = {}
+        if city:
+            d_choice['city__slug'] = city
+        if language:
+            d_choice['language__slug'] = language
+        qs = Vacancy.objects.filter(**d_choice)
+    form = VacancyFindForm
+    context = {'object_list': qs, 'form': form}
     return render(request, 'main/general.html', context)
