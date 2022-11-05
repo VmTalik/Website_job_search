78cf85afc7ca5b8d76aa0101af0e19280a490577
VmTalik <vmarlyan@mail.ru>
2022-11-05 22:03:40 +0300

    Fix job search function

--- job_search/main/job_parsers.py
+++ job_search/main/job_parsers.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from random import randint
+import re
 
 headers = [
     {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; rv:53.0) Gecko/20100101 Firefox/53.0',
@@ -24,10 +25,19 @@ def superjob(url, city=None, language=None):
             div_lst = soup.find_all('div', class_='_2lp1U _3NBrD _1WtQU')
             for div in div_lst:
                 title = div.find('span', class_='_3FqEL _2JQOY _1dTXK _3h_V4 _1hdbq').find('a')
-                jobs.append(title)
-                # дописать
+                href = ''.join(re.findall(r'/\D+\d+\Shtml', str(title)))
+                description = div.find('div', class_='_2d_Of _3NBrD _1WtQU')
+                company = div.find('div', class_='_1h4U2 _23y_P _2yUp4').find('div')
+                jobs.append({'title': title.text, 'url': domain + href,
+                             'description': description.text, 'company': company.text,
+                             'city_id': city, 'language_id': language})
+
         else:
-            errors.append({'url': url, 'title': "Div does not exists"})
+            errors.append({'url': url, 'title': "Div не существует!"})
     else:
-        errors.append({'url': url, 'title': "Page do not response"})
+        errors.append({'url': url, 'title': "Страница не найдена!"})
     return jobs
+
+# url = "https://www.superjob.ru/vacancy/search/?keywords=python&geo%5Bt%5D%5B0%5D=4"
+#
+# print(superjob(url))
